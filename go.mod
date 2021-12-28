fc919e1626aba755f35127cd20b60afb1241df11
shalldie <hi.xieshuang@gmail.com>
2021-12-28 15:23:40 +0800

    format fields

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.16
 require (
 	github.com/aws/aws-sdk-go v1.42.25
 	github.com/caarlos0/env/v6 v6.8.0
-	github.com/joho/godotenv v1.4.0 // indirect
-	github.com/manifoldco/promptui v0.9.0 // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/jedib0t/go-pretty/v6 v6.2.4
+	github.com/joho/godotenv v1.4.0
+	github.com/manifoldco/promptui v0.9.0
 )
--- s3upload/setup.go
+++ s3upload/setup.go
@@ -2,17 +2,38 @@ package s3upload
 
 import (
 	"fmt"
+	"gocli/tool"
 	"os"
 	"strings"
 
+	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/manifoldco/promptui"
 )
 
+func printOptionTable() {
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"字段名", "字段类型"})
+
+	options := &UploadOptions{}
+	fieldTuples := tool.Struct2TypeTuple(*options)
+
+	for _, tuple := range fieldTuples {
+		t.AppendRow([]interface{}{tuple[0], tuple[1]})
+	}
+	// t.SetStyle(table.StyleColoredBright)
+	t.Render()
+
+}
+
 func Setup() {
 
+	printOptionTable()
+
 	pro := promptui.Select{
-		Label: "从 【自己输入】或者【环境变量】(需要 .env 文件)配置 ",
-		Items: []string{"自己输入", "环境变量"},
+		Label: "选择【.env 文件】或者【自己输入】配置 ",
+		Items: []string{".env 文件", "自己输入"},
 	}
 	_, result, err := pro.Run()
 
@@ -22,7 +43,7 @@ func Setup() {
 
 	var options *UploadOptions
 
-	if result == "环境变量" {
+	if result == ".env 文件" {
 		options = ParseOptionsFromDotEnv(true)
 	} else {
 		options = ParseOptionsFromUI()
