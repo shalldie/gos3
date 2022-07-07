0f91a442fcce5a65a3e12fd7384d4ec8d573b814
shalldie <hi.xieshuang@gmail.com>
2022-07-07 16:53:43 +0800

    trim stacktrace

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,6 @@ import (
 	"github.com/shalldie/gos3/internal/upload"
 )
 
-type Person struct {
-	Name string
-	Age  int
-}
-
 func main() {
 	upload.Setup()
 }
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+BASE_PATH=$(
+    cd $(dirname $0)
+    pwd
+)
+
+BASE_PATH=$(
+    cd $(dirname $BASE_PATH)
+    pwd
+)
+
 export CGO_ENABLED=0
 
 TARGET_OS_NAMES=(linux-amd64 linux-arm64 darwin-amd64 darwin-arm64)
@@ -14,7 +24,7 @@ for os_name in ${TARGET_OS_NAMES[*]}; do
 
     GOOS=${tupleName[0]} \
         GOARCH=${tupleName[1]} \
-        go build -o gos3.${os_name} cmd/main.go
+        go build -gcflags "all=-trimpath=$BASE_PATH" -o gos3.${os_name} cmd/main.go
 
     mkdir -p build
     mv gos3.$os_name build/gos3.$os_name
